bfa46c31561865cbae79ba627909b1ff5d8292b0
Charley Lawton <user@CODECLAN055.local>
2016-08-10 17:22:00 +0100

    to be completed

--- music_library_controller.rb
+++ music_library_controller.rb
@@ -1,74 +1,94 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
-require_relative('./models/album')
+# require_relative('./models/album')
 require_relative('./models/artist')
 
-# albums
-post '/albums:id' do
-  Album.update(params)
+# # artist
+get '/artists/new' do
+  erb(:new)
 end
 
-get '/albums' do
-  @albums = Album.all()
-  erb(:index)
-end
-
-  get '/albums/new' do
-    erb(:new)
-  end
-
-  get 'albums/:id/edit' do
-    @album = Album.find(params[:id])
-    erb(:edit)
-  end
-
-  get '/albums/:id' do
-    @album = Album.find(params[:id])
-    erb(:show)
-  end
-
-    post '/albums' do
-      @album = Album.new(params)
-      @album.save()
-      erb(:create)
-    end
-
-  post '/albums/:id/delete' do
-    Album.delete(params[:id])
-  end
-
-# artist
-
-post '/artists:id' do
-  Artist.update(params)
-end
+# get '/artists' do
+#   @artist = Artist.new( params )
+#   @artist.save
+#   redirect(to("/artists"))
+# end
 
 get '/artists' do
   @artists = Artist.all()
-  erb(:index)
+  erb(:'artists/index')
 end
 
-  get '/artists/new' do
-    erb(:new)
-  end
-
-  get 'artists/:id/edit' do
-    @artist = Artist.find(params[:id])
-    erb(:edit)
-  end
-
-  get '/artists/:id' do
+ get '/artists/:id' do
     @artist = Artist.find(params[:id])
     erb(:show)
   end
 
-    post '/artists' do
-      @artist = Artist.new(params)
-      @artist.save()
-      erb(:create)
-    end
+# post '/artists:id' do
+#   Artist.update(params)
+# end
 
-  post '/artists/:id/delete' do
-    Artist.delete(params[:id])
-  end
+# get '/artists' do
+#   @artists = Artist.all()
+#   erb(:index)
+# end
+
+#   get '/artists/new' do
+#     erb(:new)
+#   end
+
+#   get 'artists/:id/edit' do
+#     @artist = Artist.find(params[:id])
+#     erb(:edit)
+#   end
+
+
+#     post '/artists' do
+#       @artist = Artist.new(params)
+#       @artist.save()
+#       erb(:create)
+#     end
+
+#   post '/artists/:id/delete' do
+#     Artist.delete(params[:id])
+#     redirect( to ('/artists') )
+#   end
+
+# # albums
+
+
+# post '/albums:id' do
+#   Album.update(params)
+# end
+
+# get '/albums' do
+#   @albums = Album.all()
+#   erb(:index)
+# end
+
+#   get '/albums/new' do
+#     erb(:new)
+#   end
+
+#   get 'albums/:id/edit' do
+#     @album = Album.find(params[:id])
+#     erb(:edit)
+#   end
+
+#   get '/albums/:id' do
+#     @album = Album.find(params[:id])
+#     erb(:show)
+#   end
+
+#     post '/albums' do
+#       @album = Album.new(params)
+#       @album.save()
+#       erb(:create)
+#     end
+
+#   post '/albums/:id/delete' do
+#     Album.delete(params[:id])
+#     redirect(to ('/albums'))
+#   end
+
